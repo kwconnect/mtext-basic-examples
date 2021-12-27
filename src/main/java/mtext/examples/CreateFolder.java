755640af433a955f7d03c1adba5ddd14a563f17e
Jens Ober <jensober@yahoo.de>
2021-12-27 12:18:59 +0100

    updated examples

--- src/main/java/mtext/examples/CreateFolder.java
+++ src/main/java/mtext/examples/CreateFolder.java
@@ -9,63 +9,58 @@ import mtext.examples.util.MUtil;
 
 /**
  * M/Text client API example: Create a new folder in a specified parent folder
- * @author Timo Dreier
  **/
 public class CreateFolder {
-    /**
-     * Creates a new folder in the specified parent folder
-     * @param args Command line arguments<br>
-     * args[0] = username<br>
-     * args[1] = password<br>
-     * args[2] = folder path<br>
-     * args[3] = name of the new folder
-     **/
-    public static void main(String[] args) {
-        // initializations
-        MTextClient client = null;
-        AbstractFolder folder = null;
-        // if there are two less or to much arguments
-        if (MUtil.checkArguments(args, 4)) {
-            final String name = args[0];
-            final String pwd = args[1];
-            final String folderPath = args[2];
-            final String folderName = args[3];
-            try {
-                // connect
-                client = MTextFactory.connect(name, pwd, null);
+	/**
+	 * Creates a new folder in the specified parent folder
+	 * 
+	 * @param args Command line arguments<br>
+	 *             args[0] = username<br>
+	 *             args[1] = password<br>
+	 *             args[2] = folder path<br>
+	 *             args[3] = name of the new folder
+	 **/
+	public static void main(String[] args) {
 
-                // get the specified folder by the client and the path
-                if(folderPath.equals("\\")) {
-                  folder = client.getRootFolder();
-                }
-                else {
-                  folder = client.getRootFolder().getFolderByName(folderPath);
-                }
-                // create a new folder in the specified parent folder
-                folder.createSubFolder(folderName);
-            }
-            // M/Text exception
-            catch (MTextException me) {
-                System.out.println("Can't create the folder '" +
-                    folderName + "'!");
-                me.printStackTrace();
-            }
-            // e.g. the path is wrong -> the folder is null -> Exception
-            catch (Exception e) {
-                e.printStackTrace();
-            }
-            finally {
-                if (client != null) {
-                    // close client
-                    client.close();
-                }
-            }
-        }
-        else {
-            System.out.println("M/Text client api example: CreateFolder");
-            System.out.println();
-            System.out.println("Usage: java mtext.examples.CreateFolder " +
-                "<name> <password> <folder path> <folder name>");
-        }
-    }
+		MUtil.checkArguments(args, 4, CreateFolder.class, "<name> <password> <folder path> <folder name>");
+		final String name = args[0];
+		final String pwd = args[1];
+		final String folderPath = args[2];
+		final String folderName = args[3];
+
+		// initializations
+		MTextClient client = null;
+		AbstractFolder parentFolder = null;
+		
+		try {
+			// connect
+			client = MTextFactory.connect(name, pwd, null);
+
+			// get the specified folder by the client and the path
+			if (folderPath.equals("\\")) {
+				parentFolder = client.getRootFolder();
+			}
+			else {
+				parentFolder = client.getRootFolder().getFolderByName(folderPath);
+			}
+			
+			// create a new folder in the specified parent folder
+			parentFolder.createSubFolder(folderName);
+			System.out.println("Successfully created new folder " + folderPath + "\\" + folderName);
+		}
+		catch (MTextException me) {
+			System.err.println("Failed to create the folder " + folderName);
+			me.printStackTrace();
+		}
+		// e.g. the path is wrong -> the folder is null -> Exception
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			if (client != null) {
+				client.close();
+			}
+		}
+
+	}
 }
--- src/main/java/mtext/examples/DeleteFolder.java
+++ src/main/java/mtext/examples/DeleteFolder.java
@@ -9,66 +9,62 @@ import mtext.examples.util.MUtil;
 
 /**
  * M/Text client API example: Delete a folder by its full qualified name.
- * @author Timo Dreier
  **/
 public class DeleteFolder {
-    /**
-     * Deletes a folder by the path
-     * @param args Command line arguments<br> 
-     * args[0] = user name<br> 
-     * args[1] = password<br> 
-     * args[2] = the full qualified folder name
-     **/
-    public static void main(String[] args) {
-        // initializations
-        MTextClient client = null;
-        AbstractFolder folder = null;
-        // if there are to less or to much arguments
-        if (MUtil.checkArguments(args, 3)) {
-            final String name = args[0];
-            final String pwd = args[1];
-            final String folderName = args[2];
-            try {
-                // connect
-                client = MTextFactory.connect(name, pwd, null);
-                //get the folder
-                folder = client.getRootFolder().getFolderByName(folderName);
-                if (folder instanceof Folder) {
-                    //delete the folder
-                    ((Folder)folder).delete();
-                }
-                else {
-                    System.out.println("Root folder cannot be deleted");
-                }
-            }
-            // folder exception
-            catch (FolderNotEmptyException fnee) {
-                System.out.println("Can't delete folder '" + folderName +
-                    "' (Folder is not empty) !");
-                fnee.printStackTrace();
-            }
-            // M/Text exception
-            catch (MTextException me) {
-                System.out.println("Can't delete folder '" +
-                    folderName + "'!");
-                me.printStackTrace();
-            }
-            // e.g. the path is wrong -> the folder is null -> Exception
-            catch (Exception e) {
-                e.printStackTrace();
-            }
-            finally {
-                // close client
-                if (client != null) {
-                    client.close();
-                }
-            }
-        }
-        else {
-            System.out.println("M/Text client api example: DeleteFolder");
-            System.out.println();
-            System.out.println("Usage: java mtext.examples.DeleteFolder " +
-                "<name> <password> <full qualified folder name>");
-        }
-    }
+	/**
+	 * Deletes a folder by the path
+	 * 
+	 * @param args Command line arguments<br>
+	 *             args[0] = user name<br>
+	 *             args[1] = password<br>
+	 *             args[2] = the full qualified folder name
+	 **/
+	public static void main(String[] args) {
+
+		MUtil.checkArguments(args, 3, CreateFolder.class, "<name> <password> <folderName>");
+		final String name = args[0];
+		final String pwd = args[1];
+		final String folderName = args[2];
+
+		// initializations
+		MTextClient client = null;
+		AbstractFolder folder = null;
+
+		try {
+			// connect
+			client = MTextFactory.connect(name, pwd, null);
+			
+			// get the folder
+			folder = client.getRootFolder().getFolderByName(folderName);
+			
+			if (folder instanceof Folder) {
+				// delete the folder
+				((Folder) folder).delete();
+				System.out.println("Successfully deleted folder " + folderName);
+			}
+			else {
+				System.err.println("Root folder cannot be deleted");
+			}
+		}
+		// folder exception
+		catch (FolderNotEmptyException fnee) {
+			System.out.println("Can't delete folder '" + folderName + "' (Folder is not empty) !");
+			fnee.printStackTrace();
+		}
+		// M/Text exception
+		catch (MTextException me) {
+			System.out.println("Can't delete folder '" + folderName + "'!");
+			me.printStackTrace();
+		}
+		catch (Exception e) {
+			// e.g. the path is wrong -> the folder is null -> Exception
+			e.printStackTrace();
+		}
+		finally {
+			// close client
+			if (client != null) {
+				client.close();
+			}
+		}
+	}
 }
