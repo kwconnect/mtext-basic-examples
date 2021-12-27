ff02fe2e11794fd2ecd7e18eb26c3e276f8f5478
jensober <jensober@yahoo.de>
2021-12-27 09:30:01 +0100

    Updated example

--- src/mtext/examples/CreateConnection.java
+++ src/mtext/examples/CreateConnection.java
@@ -9,38 +9,40 @@ import mtext.examples.util.MUtil;
 
 /**
  * M/Text client API example: Create a connection and close it
- * @author Timo Dreier
  **/
 public class CreateConnection {
-    /**
-     * Create a connection and close it
-     * @param args Command line arguments<br>
-     * args[0] = username<br>
-     * args[1] = password
-     **/
-    public static void main(String[] args) {
-        // initializations
-        MTextClient client = null;
-        // check whether we have correct number of arguments
-        if (MUtil.checkArguments(args, 2)) {
-            try {
-                // connect to the server with the arguments name and password
-                client = MTextFactory.connect(args[0], args[1], null);
-                System.out.println("Created the connection succesfully !");
-                client.close();
-                System.out.println("The connection is closed");
-            }
-            // the method connect(...) throws a MTextException
-            catch (MTextException me) {
-                System.out.println("Error during connecting !");
-                me.printStackTrace();
-            }
-        }
-        else {
-            System.out.println("M/Text client api example: CreateConnection");
-            System.out.println();
-            System.out.println("Usage: java mtext.examples.CreateConnection " +
-                "<name> <password>");
-        }
-    }
+	
+	/**
+	 * Create a connection and close it
+	 * 
+	 * @param args Command line arguments<br>
+	 *             args[0] = username<br>
+	 *             args[1] = password
+	 **/
+	public static void main(String[] args) {
+		
+		// initializations
+		MTextClient client = null;
+		
+		// check whether we have correct number of arguments
+		if (MUtil.checkArguments(args, 2)) {
+			try {
+				// connect to the server with the arguments name and password
+				client = MTextFactory.connect(args[0], args[1], null);
+				System.out.println("Created the connection succesfully!");
+				client.close();
+				System.out.println("The connection is closed!");
+			}
+			// the method connect(...) throws a MTextException
+			catch (MTextException me) {
+				System.out.println("Failed to connect to M/TEXT Client!");
+				me.printStackTrace();
+			}
+		}
+		else {
+			System.out.println("M/Text client api example: CreateConnection");
+			System.out.println();
+			System.out.println("Usage: java mtext.examples.CreateConnection <name> <password>");
+		}
+	}
 }
